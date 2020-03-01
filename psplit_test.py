509df54f2d79d18e5047933795829079d895b688
yanagitani <takanori.yanagitani@gmail.com>
2020-03-01 15:44:31 +0900

    more tests.

--- psplit_test.py
+++ psplit_test.py
@@ -4,3 +4,6 @@ import psplit
 
 def test_psplit_key2name():
   assert os.path.join(".", "0.txt") == psplit.psplit_key2name()
+  assert os.path.join(".", "1.txt") == psplit.psplit_key2name(1)
+  assert os.path.join("a", "1.txt") == psplit.psplit_key2name(1, "a")
+  assert os.path.join("a", "1.csv") == psplit.psplit_key2name(1, "a", ".csv")
